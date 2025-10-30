dd56245aa378c2feffc35e8758fc4d04e5b0e80a
samian96 <sam.iannazzo@yahoo.com>
2025-10-29 21:07:45 -0700

    added function / if statement

---
+++ script.js
@@ -0,0 +1,8 @@
+// function that can be called back too after use
+let name = "User"
+
+if (name == "User") {
+    let greeting = "Hello, " + name; // can not be called back to after  used here
+    console.log(greeting);
+}
+
