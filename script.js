252fcba31df29e6f872955ac2716f8236802a6e8
samian96 <sam.iannazzo@yahoo.com>
2025-10-29 21:24:15 -0700

    added function inside of a function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // function that can be called back too after use
-let name = "User"
+let name = "User";
 
 if (name == "User") {
     let greeting = "Hello, " + name; // can not be called back to after  used here
@@ -8,5 +8,15 @@ if (name == "User") {
 
 let r = 4; // assigning radius to 4
 let res = 4 * Math.PI * r; // calculates the circumference using a formula
-console.log(`${res.toFixed(3)}`)
+console.log(`${res.toFixed(3)}`);
+
+function printHello() {
+    console.log("Hello ");
+}
+function printWorld() {
+    console.log("World");
+    printHello(); // function calling to another function
+} // The order that it printed for me went Hello , World, Hello
+printHello();
+printWorld();
 
